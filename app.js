cc42b6d0e0d26b5d527237048689d9f1611c0bee
RyryTheGuy <ryanroemer61@gmail.com>
2022-04-24 13:23:44 -0500

    Added styles to light theme

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const projectSection = document.getElementById('projects');
+const expandIcon = document.getElementById('expandToggle').children.item(0);
+console.log('Icon', expandIcon);
+
 projectSection.style.setProperty('--originalHeight', `${projectSection.scrollHeight}px`);
 
 document.getElementById('expandToggle').addEventListener('click', function () {
   projectSection.classList.toggle('expand');
+  expandIcon.classList.toggle('fa-chevron-up');  
+  expandIcon.classList.toggle('fa-chevron-down');  
 })
